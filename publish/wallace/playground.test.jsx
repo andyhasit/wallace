766bbe6eec52489882063bdb1a0d472cc9d5a3e0
Andrew Buchan <andyhasit@gmail.com>
2023-08-17 00:25:38 +0100

    tidy up

--- publish/wallace/playground.test.jsx
+++ publish/wallace/playground.test.jsx
@@ -1,16 +1,7 @@
+import {Component} from 'wallace'
 
-// const x = 10
-
-// const Child = <span>{x}</span>
-
-// const items = [1, 4, 6]
-
-// const MyComponent2 = 
-//   <div>
-//     <h1>Hello</h1>
-//     <Child _for={items}/>
-//   </div>
-
-//<this.foo _props={{foo: 'bar'}}/>
-
-const Foo = <button _on:Click={andrew.foo(david) || p.aaa}>Click me</button>
+class Test extends Component {
+  html = <div>
+    <span>{c.count}</span>
+  </div>
+}
