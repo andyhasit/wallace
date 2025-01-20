d06c8d48d8a347dfb21d067255b81db2c037adb7
Andrew Buchan <andyhasit@gmail.com>
2025-01-20 17:37:25 +1300

    added basic test

---
+++ packages/wallace/tests/functionality/14.reactivity.spec.jsx
@@ -0,0 +1,28 @@
+import { testMount } from "../utils";
+import { createProxy } from "wallace";
+
+test("Can make a component reactive", () => {
+  const MyComponent = ({ checked }) => (
+    <div>
+      <input ref:cbx type="checkbox" bind={checked} />
+      <span>{checked ? "yep" : "nope"}</span>
+    </div>
+  );
+  MyComponent.prototype.setProps = function (props) {
+    this.props = createProxy(props, this);
+    this.update();
+  };
+  const component = testMount(MyComponent, { checked: false });
+  expect(component).toRender(
+    `<div><input type="checkbox"><span>nope</span></div>`,
+  );
+  const checkbox = component.ref.cbx;
+  checkbox.click();
+  expect(component).toRender(
+    `<div><input type="checkbox"><span>yep</span></div>`,
+  );
+  checkbox.click();
+  expect(component).toRender(
+    `<div><input type="checkbox"><span>nope</span></div>`,
+  );
+});
